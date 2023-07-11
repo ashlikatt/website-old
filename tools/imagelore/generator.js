b217aef6f009a5e35f01e53da6d1d6efeb95d40d
Ashli Katt <sqrtseven7@gmail.com>
2023-07-11 15:41:48 -0500

    Add lore generator

---
+++ tools/imagelore/generator.js
@@ -0,0 +1,144 @@
+// When the page loads...
+addEventListener('load', function() {
+
+    // When the selected file changes 
+    document.getElementById('inputfile').addEventListener('change', inputChanged);
+
+    // When the selected quality changes 
+    document.getElementById('inputrange').addEventListener('change', inputChanged);
+
+    inputChanged();
+    invalidate();
+});
+
+var inputImage = undefined;
+var imageBitmap = undefined;
+var imageBitmapURL = undefined;
+var tellraw = undefined;
+var valid = false;
+
+function inputChanged() {
+    try {
+        // Get file input
+        const file = document.getElementById('inputfile').files[0];
+
+        // Get quality input
+        const quality = parseInt(document.getElementById('inputrange').value) - 1;
+    
+        // Validate input
+        if (!(file instanceof File)) throw new Error("Input file is invalid - invalid data.");
+        if (!file.type.startsWith("image")) throw new Error("Input file is invalid - not an image.");
+        if (quality < 0 || quality > 99) throw new Error("Input quality is invalid - out of range.");
+
+        inputImage = URL.createObjectURL(file);
+        createImageBitmap(file).then(bitmap => {
+            const maxWidth = Math.min(100, bitmap.width);
+            const maxHeight = Math.min(100, bitmap.height);
+            const targetWidth = interpInt(5, maxWidth, quality/100);
+            const targetHeight = interpInt(5, maxHeight, quality/100);
+            
+            const canvas = document.createElement("canvas");
+            canvas.width = targetWidth;
+            canvas.height = targetHeight;
+            const context = canvas.getContext("2d");
+            context.drawImage(bitmap, 0, 0, targetWidth, targetHeight)
+            imageBitmap = context.getImageData(0, 0, targetWidth, targetHeight);
+            imageBitmapURL = canvas.toDataURL();
+            canvas.remove();
+
+            updateOutput();
+        });
+    } catch (e) {
+        invalidate();
+        console.log(e);
+    }
+}
+
+function updateOutput() {
+    if (inputImage) {
+        const img = new Image();
+        img.src = inputImage;
+        const display = document.getElementById("imageDisplay");
+        display.innerHTML = "";
+        display.appendChild(img);
+    }
+
+    if (imageBitmapURL) {
+        const img = new Image();
+        img.src = imageBitmapURL;
+        img.id = "pixelImage";
+        const display = document.getElementById("pixelDisplay");
+        display.innerHTML = "";
+        display.appendChild(img);
+    }
+
+    const output = document.getElementById("output")
+    if (imageBitmap) {
+        const data = imageBitmap.data;
+        let currentIndex = 0;
+        const lines = [];
+        for (let line = 0; line < imageBitmap.height; line++) {
+            let lineData = [];
+            for (let column = 0; column < imageBitmap.width; column++) {
+                let r = data[currentIndex++];
+                let g = data[currentIndex++];
+                let b = data[currentIndex++];
+                currentIndex++;
+                if (lineData.length === 0) {
+                    lineData.push('{"italic":false,"color":"' + hex(r, g, b) + '","text":"█"}');
+                } else {
+                    lineData.push('{"color":"' + hex(r, g, b) + '","text":"█"}');
+                }
+            }
+            lines.push("'[" + lineData.join(",") + "]'");
+        }
+        tellraw = "[" + lines.join(",") + "]";
+
+        validate();
+    } else {
+        invalidate();
+    }
+}
+
+function interpInt(min, max, percent) {
+    return Math.round(((max-min)*percent)+min)
+}
+
+function hex(r, g, b) {
+    return "#" + 
+        r.toString(16).padStart(2, "0") +
+        g.toString(16).padStart(2, "0") +
+        b.toString(16).padStart(2, "0")
+}
+
+function invalidate() {
+    valid = false;
+    document.getElementById("recodeButton").classList.add("disabled");
+    document.getElementById("copyButton").classList.add("disabled");
+    console.log("invalidate")
+}
+
+function validate() {
+    valid = true;
+    document.getElementById("recodeButton").classList.remove("disabled");
+    document.getElementById("copyButton").classList.remove("disabled");
+    console.log("validate")
+}
+
+function copyButton() {
+    if (!valid) return;
+    const nbt = "{display:{Lore:" + tellraw + "}}"
+    const command = "/give @p minecraft:stone" + nbt;
+    navigator.clipboard.writeText(command);
+}
+
+function recodeButton() {
+    if (!valid) return;
+    const packet = `{"source":"Ashli's Site","type":"nbt","data":"{'id':'minecraft:stone','Count':1,tag:{display:{Lore:${tellraw.replaceAll('"', '\\"')}}}}"}`;
+    console.log(packet);
+    const ws = new WebSocket("ws://localhost:31371");
+    ws.addEventListener('open', e => {
+        console.log("a")
+        ws.send(packet);
+    })
+}
