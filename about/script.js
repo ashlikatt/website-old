abf52287fd20bdcb9a04613b0115ce2125bbbd94
Ashli Katt <sqrtseven7@gmail.com>
2023-10-17 23:08:25 -0500

    redesign

--- about/script.js
+++
@@ -1,5 +0,0 @@
-addEventListener('load', function() {
-    const diff = (new Date() - new Date('2004-11-12T00:00:00.000-05:00'));
-    const time = Math.floor(diff / 1000 / 60 / 60 / 24 / 365);
-    document.getElementById('age').innerHTML = 'I am ' + time + ' years old. ';
-});
